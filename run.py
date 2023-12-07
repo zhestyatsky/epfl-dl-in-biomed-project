f73f466138c71f7f3ec81a4530e37b961af4aa4e
Boris Zhestiankin <boris.jestiankin@icloud.com>
2023-12-07 10:41:45 +0100

    add weight_decay

--- run.py
+++ run.py
@@ -106,9 +106,9 @@ def train(train_loader, val_loader, model, cfg):
 
     if cfg.method.name == "leo":
         model_parameters = [*model.encoder.parameters(), *model.decoder.parameters(), model.inner_lr, model.finetuning_lr]
+        optimizer = instantiate(cfg.optimizer_cls, params=model_parameters, weight_decay=cfg.method.weight_decay)
     else:
-        model_parameters = model.parameters()
-    optimizer = instantiate(cfg.optimizer_cls, params=model_parameters)
+        optimizer = instantiate(cfg.optimizer_cls, params=model.parameters())
 
     print("Model Architecture:")
     print(model)
