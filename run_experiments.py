fda36eb727f24c65c2d56dd1ce817fe2427e6f45
Said Gurbuz <said.gurbuz@epfl.ch>
2023-12-08 22:54:55 +0000

    update grid search script

--- run_experiments.py
+++ run_experiments.py
@@ -3,26 +3,10 @@ import os
 
 # grid search configurations
 configurations = [
-    {'method': 'baseline', 'dataset': 'swissprot', 'n_shot': 5},
-    {'method': 'baseline', 'dataset': 'swissprot', 'n_shot': 1},
-    {'method': 'baseline_pp', 'dataset': 'swissprot', 'n_shot': 5},
-    {'method': 'baseline_pp', 'dataset': 'swissprot', 'n_shot': 1},
-    {'method': 'maml', 'dataset': 'swissprot', 'n_shot': 5},
-    {'method': 'maml', 'dataset': 'swissprot', 'n_shot': 1},
-    {'method': 'matchingnet', 'dataset': 'swissprot', 'n_shot': 5},
-    {'method': 'matchingnet', 'dataset': 'swissprot', 'n_shot': 1},
-    {'method': 'protonet', 'dataset': 'swissprot', 'n_shot': 5},
-    {'method': 'protonet', 'dataset': 'swissprot', 'n_shot': 1},
-    {'method': 'baseline', 'dataset': 'tabula_muris', 'n_shot': 5},
-    {'method': 'baseline', 'dataset': 'tabula_muris', 'n_shot': 1},
-    {'method': 'baseline_pp', 'dataset': 'tabula_muris', 'n_shot': 5},
-    {'method': 'baseline_pp', 'dataset': 'tabula_muris', 'n_shot': 1},
-    {'method': 'maml', 'dataset': 'tabula_muris', 'n_shot': 5},
-    {'method': 'maml', 'dataset': 'tabula_muris', 'n_shot': 1},
-    {'method': 'matchingnet', 'dataset': 'tabula_muris', 'n_shot': 5},
-    {'method': 'matchingnet', 'dataset': 'tabula_muris', 'n_shot': 1},
-    {'method': 'protonet', 'dataset': 'tabula_muris', 'n_shot': 5},
-    {'method': 'protonet', 'dataset': 'tabula_muris', 'n_shot': 1},
+    {'dataset': 'swissprot', 'n_shot': 5},
+    {'dataset': 'swissprot', 'n_shot': 1},
+    {'dataset': 'tabula_muris', 'n_shot': 5},
+    {'dataset': 'tabula_muris', 'n_shot': 1},
     # configurations will be here
 ]
 
@@ -38,7 +22,7 @@ os.makedirs(output_dir, exist_ok=True)
 for config in configurations:
     # Update exp.name according to other options
     exp_name = '_'.join([f"{key}_{value}" for key, value in config.items()])
-    command = f"python {run_script_path} exp.name={exp_name} {' '.join([f'{key}={value}' for key, value in config.items()])}"
+    command = f"python {run_script_path} exp.name={exp_name} method=leo {' '.join([f'{key}={value}' for key, value in config.items()])}"
 
     # Create the output file name based on the configuration
     output_file = f"{output_dir}output_{exp_name}.txt"
