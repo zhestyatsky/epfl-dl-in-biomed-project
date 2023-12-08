476d3954efd88fbcc8027ae70d9041848702f86b
zhestyatsky <49404515+zhestyatsky@users.noreply.github.com>
2023-12-08 23:49:38 +0100

    Seed more things (#6)
    Co-authored-by: Boris Zhestiankin <boris.jestiankin@icloud.com>

--- utils/io_utils.py
+++ utils/io_utils.py
@@ -55,9 +55,13 @@ def get_model_file(cfg):
 
 
 def fix_seed(seed=42):
+    os.environ['PYTHONHASHSEED'] = str(seed)
     np.random.seed(seed)
     torch.manual_seed(seed)
     torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
     random.seed(seed)
 
 
