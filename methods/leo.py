6fa5f1cc4426b282a3d25a5a24671a26c9b4564b
Boris Zhestiankin <boris.jestiankin@icloud.com>
2023-12-08 22:17:59 +0100

    print grad in inner loop

--- methods/leo.py
+++ methods/leo.py
@@ -252,6 +252,7 @@ class LEO(MetaTemplate):
             scores = self.forward(x_support)
             set_loss = self.loss_fn(scores, y_support)
             grad = torch.autograd.grad(set_loss, latents_z, create_graph=True)[0]
+            print(grad)
             latents_z = latents_z - self.inner_lr * grad
             weights = self.decoder(latents_z)
             self.set_weights(weights)
