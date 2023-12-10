664b73528842777999491a7222eb5cd05a12ed86
Boris Zhestiankin <boris.jestiankin@icloud.com>
2023-12-10 23:49:02 +0100

    add correct func print

--- methods/meta_template.py
+++ methods/meta_template.py
@@ -59,6 +59,9 @@ class MetaTemplate(nn.Module):
 
         topk_scores, topk_labels = scores.data.topk(1, 1, True, True)
         topk_ind = topk_labels.cpu().numpy()
+        print("topk_ind", topk_ind)
+        print("topk_ind[:, 0]", topk_ind[:, 0])
+        print("y_query", y_query)
         top1_correct = np.sum(topk_ind[:, 0] == y_query)
         return float(top1_correct), len(y_query)
 
